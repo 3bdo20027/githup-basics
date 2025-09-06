947b16135367c713f96d4a39f39c1be137ed9292
3bdo20027 <abdelbadeeyehia@gmail.com>
2025-09-06 15:54:26 +0300

    k
    ff

---
+++ main.py
@@ -0,0 +1,31 @@
+from camera_setup import get_capture,get_farme
+from hand_deyection import process_lm,drawing_landmarks
+import cv2
+
+cap=get_capture()
+
+while True:
+
+    frame=get_farme(cap)
+
+    results=process_lm(frame)
+    frame=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks:
+
+        frame=drawing_landmarks(img=frame,results=results)
+
+
+
+    cv2.imshow('main programe',frame)  
+
+    if cv2.waitKey(2) & 0xff==ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+  
+
+
+
